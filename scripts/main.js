778f9934fc22da6f8ca7936a0c99ffc72def2dc9
1amks <maxik.wan@yandex.ru>
2023-06-13 00:55:07 +0300

    polyfinal!

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector(".right").classList.toggle("open")
-    })
-})
-window.addEventListener('keydown', (e) => {
-    if (e.key === "Escape") {
-        // Действие при клике
-        document.querySelector(".right").classList.remove("open")
-    }
-});
-document.getElementById("nav").addEventListener('click', event => {
-    event._isClickWithInMenu = true;
-});
-document.getElementById("burger").addEventListener('click', event => {
-    event._isClickWithInMenu = true;
-});
-document.body.addEventListener('click', event => {
-    if (event._isClickWithInMenu) return;
-    // Действие при клике
-    document.querySelector(".right").classList.remove("open")
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
