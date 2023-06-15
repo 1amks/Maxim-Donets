6d38f9abecece33cdbfc0e828db0f66d15fbc1bb
1amks <maxik.wan@yandex.ru>
2023-06-15 07:35:03 +0300

    melo4

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $headers =	'From: no-reply@example.com' . "\r\n" .
 			'Reply-To: webmaster@example.com' . "\r\n" .
 			'X-Mailer: PHP/' . phpversion();
 if (isset($_POST['name']) && isset($_POST['phone'])&& isset($_POST['text']))
-	if (mail("xxx.gmail.com", $_POST['text'], $mess, $headers)) {
+	if (mail("maxik.wan.yandex.ru", $_POST['text'], $mess, $headers)) {
 		header("Location: index.html");
 	} else {
 		header("Location: index.html");
--- script.js
+++ script.js
@@ -84,3 +84,15 @@ function active(i) {
     btn.classList.remove('menu-btn_active')
     
 }
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(event) {
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
